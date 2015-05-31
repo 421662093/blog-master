ca292df39609189fdd9ea4caebcf41125979c940
clr <c>
2015-05-31 15:39:15 +0800

    clon

---
+++ config_default.py
@@ -0,0 +1,20 @@
+configs = {
+    'db':{
+        'host':'127.0.0.1',
+        'port':3306,
+        'user':'jiffies',
+        'password':'jiffies',
+        'database':'jblog'
+    }, 
+    'session':{
+        'secret':'AwEsOmE'    
+    },
+    'review':{
+        'domain':'code4awesome.sinaapp.com',
+        'url':'http://v2.uyan.cc/code/uyan.js?uid=2011427'
+        },
+    'storage':{
+        'bucket':'code4awesome'
+        }
+        
+}
